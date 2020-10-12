000a74e654b1a51c50e3e69dc43dad7bd752d19a
Terrence Wong <terrencewong223@gmail.com>
2020-10-12 11:33:49 -0700

    passed all tests for foragerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,13 +1,12 @@
 var ForagerBee = function() {
   Bee.call(this);
   this.age = 10;
-  this.job = 'make honey';
-  this.honeyPot = 0;
-  this.makeHoney = function() {
-    this.honeyPot++;
-  }
-  this.giveHoney = function() {
-    this.honeyPot--;
-  }
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
-
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
